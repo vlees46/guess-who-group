96fef8fefdb8a589b59c019b78dace0683cbea3a
Victor Lees <vlees14.pa@gmail.com>
2021-11-09 09:57:45 -0500

    Initial Commit created divs

--- client.js
+++ client.js
@@ -1,14 +1,30 @@
 
+// document load, append divs for each person in the people array (it comes from `data.js` file) to the DOM with jQuery 
+// each containing an image from each GitHub profile
 
 $(document).ready(readyNow);
 
 
 function readyNow() {
 
-$('#guessPic').append(`<div>${people[0].name}</div>`)
-$('#guessPic').append(`<div>${people[1].name}</div>`)
-$('#guessPic').append(`<div>${people[2].name}</div>`)
+    // Handler for .load() called.
+  let totalPeople = [];
 
+   for ( list of people ) {
+
+    console.log('display cars in the garage');
+    // target output by ID
+    let totalPeople = $('#test');
+    // empty
+    totalPeople.empty();
+    // loop through garage array
+    for ( list  of people  ) {
+      // for each car, create a list item, animate and play sound
+       totalPeople.append( `<div> ${list.name}</div>`);
+       totalPeople.append( `<img src='#pic1'</img>`);
+     
+   }
+
+} 
 }
-
-console.log('Here are all the available people:', people);
+//console.log('Here are all the available people:', people);
